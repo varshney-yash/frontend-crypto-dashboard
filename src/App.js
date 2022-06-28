bfd5494f2455c9451b7d4351a81da7d9b1c53215
Yash Varshney <107148830+varshney-yash@users.noreply.github.com>
2022-06-28 23:16:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NewsFeed from "./components/NewsFeed";
+import CurrencyConv from "./components/CurrencyConv";
+
+function App() {
+  return (
+    <div className="app">
+      <CurrencyConv/>
+      <NewsFeed/>
+    </div>
+  );
+}
+
+export default App;
